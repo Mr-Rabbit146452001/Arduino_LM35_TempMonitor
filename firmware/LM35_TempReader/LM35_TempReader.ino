2a3eea11da7b8f2e49f3341422eee31ef2dc51be
DepressedCatpk <nguyenkhoi30122005@gmail.com>
2026-04-15 12:26:57 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,23 +1,27 @@
 /*
- * Du an: Doc nhiet do LM35 va gui qua Serial (dinh dang JSON)
- * Mon: Mang Cam Bien - Tuan 03
+ * Dự án: Đọc nhiệt độ 2 kênh LM35 và gửi qua Serial dạng CSV
+ * Môn: Mạng Cảm Biến - Tuần 03
  */
 
-int adcValue;
-float nhietDo;
+int adcValues[2]; 
+float nhietDo[2]; 
+char chuoi[30];
 
 void setup() {
-    Serial.begin(9600);
-    delay(100);
+  Serial.begin(9600);
+  delay(100);
 }
 
 void loop() {
-    adcValue = analogRead(A0);
-    nhietDo = (adcValue * 500.0) / 1023.0;
-    
-    Serial.print("{\"temperature\":");
-    Serial.print(nhietDo, 1);
-    Serial.println("}");
-    
-    delay(1000);
+  // SV B tối ưu code, bỏ mảng và dùng biến đơn
+  int adcValue = analogRead(A0);
+  float nhietDo = (adcValue * 500.0) / 1023.0;
+
+  // Đổi định dạng xuất ra Serial sang chuẩn JSON
+  Serial.print("{\"temperature\":");
+  Serial.print(nhietDo, 1); // In ra 1 chữ số thập phân
+  Serial.println("}");
+
+  delay(100);
+}
 }
