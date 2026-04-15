c55d705a68be35c4c697b8dbc0cd5f20f7e297e8
Mr-Rabbit146452001 <levanhungu652001@gmail.com>
2026-04-15 12:15:13 +0700

    feat: them kenh A2

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,10 +1,11 @@
 /*
- * Du an: Doc nhiet do LM35 va gui qua Serial (dinh dang JSON)
+ * Du an: Doc nhiet do LM35 va gui qua Serial (3 kenh A0, A1, A2)
  * Mon: Mang Cam Bien - Tuan 03
  */
 
-int adcValue;
-float nhietDo;
+int adcValues[3];
+float nhietDo[3];
+char chuoi[50];
 
 void setup() {
     Serial.begin(9600);
@@ -12,12 +13,17 @@ void setup() {
 }
 
 void loop() {
-    adcValue = analogRead(A0);
-    nhietDo = (adcValue * 500.0) / 1023.0;
+    adcValues[0] = analogRead(A0);
+    nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
     
-    Serial.print("{\"temperature\":");
-    Serial.print(nhietDo, 1);
-    Serial.println("}");
+    adcValues[1] = analogRead(A1);
+    nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
     
-    delay(1000);
+    adcValues[2] = analogRead(A2);
+    nhietDo[2] = (adcValues[2] * 500.0) / 1023.0;
+    
+    sprintf(chuoi, "%d,%d,%d\n", (int)nhietDo[0], (int)nhietDo[1], (int)nhietDo[2]);
+    Serial.print(chuoi);
+    
+    delay(100);
 }
